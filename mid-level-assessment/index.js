b7830b1a3d63d8d04e05f0b0e6bab29ab751fe61
thereIsNoSpoon8891 <130624304+thereIsNoSpoon8891@users.noreply.github.com>
2023-10-01 11:45:42 -0700

    add notes

--- mid-level-assessment/index.js
+++ mid-level-assessment/index.js
@@ -1,4 +1,46 @@
 /*
+## Code Challenges
+
+Challenge 1: **Sort Array with Preserved Index for -1 Values**
+
+You are given an array a containing integers. Your task is to implement the solution function to sort the array in ascending order, while preserving the original index positions of -1 values.
+
+Write a function solution(a) that takes in an array a and returns a new array with the following conditions:
+
+All non -1 values in the array should be sorted in ascending order.
+The -1 values should retain their original index positions in the sorted array.
+*/
+
+function solution (array) {
+
+    // 1. clone the array to avoid mutating the original array
+    const sortedArray = array.slice()
+
+
+    // 2. filter out the -1 elements and sort the remaining numbers
+    const sortedNums = sortedArray.filter(num => num !== -1).sort((a, b) => a - b)
+
+
+    // 3. keep track of the current index in the sorted array
+    let sortedindex = 0
+    
+    // 4. iterate over the original array and replace non-negative numbers with sorted ones
+    for (i = 0; i < sortedArray.length; i++) {
+        if (sortedArray[i] !== -1){// re-assign the sorted numbers back into the array, unless it is -1
+            sortedArray[i] = sortedNums[sortedindex]
+            sortedindex++
+        }
+    }
+    return sortedArray
+}
+// console.log(solution([-1, 150, 190, 170, -1, -1, 160, 180]))
+// console.log(solution([5, 3, 1]))
+// console.log(solution([-1, -1, -1, -1]))
+//  console.log(solution([100, -1, 50, -1, 75]))
+
+
+
+/*
 Write a JavaScript function called **`findSumOfTwo`** that takes in an array of numbers and a target number. 
 The function should find two numbers in the array that add up to the target number and return them as an array. 
 If there are multiple pairs that satisfy the condition, return any one of them. If no such pair exists, return an empty array.
@@ -33,7 +75,7 @@ function findSumOfTwo(array, target){
     return result
 }
 
-console.log(findSumOfTwo(numbers, target))
+//console.log(findSumOfTwo(numbers, target))
 // console.log(findSumOfTwo(moreNumbers, target))
 
 /*
@@ -69,5 +111,6 @@ let count = 0
    }
    return count
 }
-console.log(countVowels(input))
-console.log(countVowels(vowels))
+// console.log(countVowels(input))
+// console.log(countVowels(vowels))
+
